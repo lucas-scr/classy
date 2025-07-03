1ee7a202bbd4f7448cc2221886fd7256e0740309
Lucas <lucas.carvalhu.ferreira@gmail.com>
2025-07-03 11:13:37 -0300

    alterado chave publica do supabase para chave publishable

--- src/app/core/services/SupabaseService.ts
+++ src/app/core/services/SupabaseService.ts
@@ -10,7 +10,7 @@ export class SupabaseService {
   constructor() {
     this.supabase = createClient(
       'https://cknedrdeyzjseckxspqx.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbmVkcmRleXpqc2Vja3hzcHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyODI0MzgsImV4cCI6MjA2NTg1ODQzOH0.0WzXarBa41fl7mB0NorAlm66kU69BCM6Ct_EPtEr0UI'
+      'sb_publishable_EwMJmI0Prj2CsOuul2KjgQ_5sGSBFrJ'
     );
   }
 
